62d735bebf7d36f86e328de25e94c434bdf5095c
ITPLUS Academy <thanghoang064@gmail.com>
2023-09-18 13:45:42 +0700

    update project

--- bai2_mvc/index.php
+++ bai2_mvc/index.php
@@ -5,6 +5,14 @@ switch ($url) {
     case '/':
         listProduct();
     break;
+    case 'add-product':
+        addProduct();
+    break;
+    case 'edit-product':
+        editProduct();
+    break;
     // nếu như case = add-product trỏ vào hàm addProduct
     // nếu như case = edit-product trỏ vào hàm editProduct
+    // tạo 1 controller CustomerController và gọi case ở trang
+    // này luôn  làm y như product
 }
