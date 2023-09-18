4b1e27bc1603585163510bf530f3f368ca0abf7b
ITPLUS Academy <thanghoang064@gmail.com>
2023-09-18 12:57:23 +0700

    update project

--- bai1/ontap1.php
+++ bai1/ontap1.php
@@ -19,10 +19,21 @@ ktSoChan($b);
 // thì đầu ra sẽ tự xử lý
 // xây dựng 1 hàm không trả về tính diện tích hình chữ nhật với
 // hai tham số truyền vào là chiều dài và chiều rộng
+// loại không trả về  (Không thấy có chữ return ở trong hàm
 function dienTichHCN($chieuDai,$chieuRong) {
     echo "Diện tích là" .$chieuDai * $chieuRong;
 }
+// loại có trả về (Loại có return )
+function dienTichHCNCoTraVe($chieuDai,$chieuRong) {
+    return $chieuDai * $chieuRong;
+}
+//cách gọi hàm không trả về thì chỉ cần gọi tên hàm và truyền giá trị
+// tương ứng với tham số
 dienTichHCN(4,5);
+//cách gọi hàm có trả về
+echo dienTichHCNCoTraVe(4,5) + 10;
+
+//false;
 // xây dựng 1 hàm không trả về truyền vào những tham số sau
 //tên,năm sinh,địa chỉ,email
 // hàm này sẽ xử lý hiển thị ra thông tin tên + tuổi + địa chỉ + Email
