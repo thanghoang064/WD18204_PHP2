db3e5c17271cb905c8aa96c8dbb04e526a30ecd9
ITPLUS Academy <thanghoang064@gmail.com>
2023-09-24 08:25:35 +0700

    update project

--- bai3_oop/oop.php
+++ bai3_oop/oop.php
@@ -2,11 +2,23 @@
 //khai báo class
 class SinhVien {
     // khai báo thuộc tính tức là khai báo biến trong class
-   public $tenSV  ;
+   public $tenSV;
    public $maSV;
    public $namSinh;
-
+    //hàm khởi tạo hoặc phương thức khởi tạo
+    // phương thức khởi tạo là phương thức đặc biệt được xếp vào thành 1 dạng
+    // magic function và nó chỉ được gọi ngay khi khởi tạo đối tượng
    // phương thức set giá trị cho tên
+    // có 2 loại phương thức khởi tạo
+    // - phương thức khởi tạo có tham số truyền vào
+    // thì chúng ta sẽ phải giá trị cho nó luôn khi chúng ta khởi tạo đối tượng
+    public function __construct($tenSV,$maSV,$namSinh) {
+       //mình muốn khi new đối tượng nó sẽ xử lý 1 cái logic bất kỳ trong này
+        $this->tenSV  = $tenSV;
+        $this->maSV = $maSV;
+        $this->namSinh = $namSinh;
+    }
+    // - phương thức khởi tạo không có tham số truyền vào
    public function setTen($ten) {
        $this->tenSV = $ten;
    }
@@ -19,14 +31,14 @@ class SinhVien {
     }
 }
 
-$sv1 = new SinhVien(); // khởi tạo đối tượng sinh viên có nghĩa là
+$sv1 = new SinhVien("Bùi lệ quỳnh","SV001",2003); // khởi tạo đối tượng sinh viên có nghĩa là
 // tạo ra 1 ông sinh viên mới
 $sv1->setTen("Quang");
 $sv1->hienThiThongTinSV();
-echo "<br>";
-$sv2 = new SinhVien(); // tạo ra 1 ôn g sinh viên 2
-$sv2->setTen("Hiếu");
-$sv2->hienThiThongTinSV();
+//echo "<br>";
+//$sv2 = new SinhVien(); // tạo ra 1 ôn g sinh viên 2
+//$sv2->setTen("Hiếu");
+//$sv2->hienThiThongTinSV();
 
 //tạo 1 class GiangVien gồm các thuộc tính :
 // maGV,tenGV,namBatDau,luongCB,soGioDay
