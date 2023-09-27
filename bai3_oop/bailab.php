a4ebc609be9d4f3e0ef795ec3dd28508eb9c1fb1
ITPLUS Academy <thanghoang064@gmail.com>
2023-09-27 13:09:07 +0700

    update project

--- bai3_oop/bailab.php
+++ bai3_oop/bailab.php
@@ -13,3 +13,70 @@
 /// tạo phương thức tính tổng lương = luongCB *soGioDay
 /// //tạo phương thức hiển thị thông tin giảng viên hiển thị ra các thông tin
 //// hoten,diachi,tuoi,email,sdt,tổng lương
+class ConNguoi {
+    public $hoTen;
+    public $diaChi;
+    public $namSinh;
+    public $email;
+    public $sdt;
+    public function __construct($hoTen,$namSinh,$sdt) {
+        $this->hoTen = $hoTen;
+        $this->namSinh = $namSinh;
+        $this->sdt = $sdt;
+    }
+    public function setHoTen($hoTen) {
+        $this->hoTen = $hoTen;
+    }
+    public function setNamSinh($namSinh) {
+        $this->namSinh = $namSinh;
+    }
+    public function setSDT($sdt) {
+        $this->sdt = $sdt;
+    }
+    public function tinhTuoi() {
+        return date('Y') - $this->namSinh;
+    }
+    public function hienThiThongTin() {
+        echo $this->hoTen."-".$this->namSinh."-".$this->tinhTuoi();
+    }
+
+}
+class HocSinh extends  ConNguoi {
+    public $diemToan;
+    public $diemLy;
+    public $diemHoa;
+    public function __construct($hoTen, $namSinh, $sdt,$diemToan,$diemLy,$diemHoa)
+    {
+        // gọi lên luôn hàm contruct của tk cha để set giá trị cho
+        // những thuộc tính ở class cha
+        parent::__construct($hoTen, $namSinh, $sdt);
+        $this->diemToan = $diemToan;
+        $this->diemLy = $diemLy;
+        $this->diemHoa  = $diemHoa;
+    }
+
+    public function setDiemToan($diemToan) {
+        $this->diemToan = $diemToan;
+    }
+    public function setDiemLy($diemLy) {
+        $this->diemLy = $diemLy;
+    }
+    public function setDiemHoa($diemHoa) {
+        $this->diemHoa = $diemHoa;
+    }
+    public function tinhDiemTB () {
+        return ($this->diemToan + $this->diemLy + $this->diemHoa)/3;
+    }
+    public function hienThiThongTinSV() {
+        $this->hienThiThongTin(); // gọi hàm hiển thị thông tin của thằng cha
+        echo $this->tinhDiemTB();
+    }
+}
+$hs = new HocSinh("Nguyễn Văn A",2003,"09222222",7,9,10);
+//$hs->setHoTen("Quang");
+//$hs->setNamSinh(2003);
+//$hs->setDiemToan(10);
+//$hs->setDiemLy(5);
+//$hs->setDiemHoa(7);
+$hs->hienThiThongTinSV();
+//làm tương tự với giảng viên
